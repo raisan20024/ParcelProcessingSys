c7d3460d26f23dd3344d8087cf5c1347b3238fc1
raisan20024 <raisan20024@gmail.com>
2024-11-25 11:57:34 +0545

    Added methods and attributes to QueuOfCustomer class

--- ParcelProcessing/src/main/QueueOfCustomers.java
+++ ParcelProcessing/src/main/QueueOfCustomers.java
@@ -1,5 +1,34 @@
 package main;
 
-public class QueueOfCustomers {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class QueueOfCustomers {
+    private Queue<Customer> clientQueue;
+    public QueueOfCustomers() {
+        this.clientQueue = new LinkedList<>();
+    }
+    public void addCustomer(Customer client) {
+        clientQueue.offer(client);
+    }
+    public Customer removeCustomer() {
+        return clientQueue.poll();
+    }
+    public Customer peekNextCustomer() {
+        return clientQueue.peek();
+    }
+    public boolean isEmpty() {
+        return clientQueue.isEmpty();
+    }
+    public int getSize() {
+        return clientQueue.size();
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Customer client : clientQueue) {
+            sb.append(client).append("\n");
+        }
+        return sb.toString();
+    }
 }
