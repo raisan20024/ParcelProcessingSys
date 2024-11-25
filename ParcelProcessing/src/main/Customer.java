0edf5cfc00419145c5caf98c721d2bbc93930e54
raisan20024 <raisan20024@gmail.com>
2024-11-25 11:44:35 +0545

    Added attribute inside Customer class

--- ParcelProcessing/src/main/Customer.java
+++ ParcelProcessing/src/main/Customer.java
@@ -1,5 +1,25 @@
 package main;
 
 public class Customer {
-
+    private int queueNumber;
+    private String name;
+    private String packageID;
+    public Customer(int queueNumber, String name, String packageID) {
+        this.queueNumber = queueNumber;
+        this.name = name;
+        this.packageID = packageID;
+    }
+    public int getQueueNumber() {
+        return queueNumber;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getPackageID() {
+        return packageID;
+    }
+    @Override
+    public String toString() {
+        return "Queue Number: " + queueNumber + ", Name: " + name + ", Package ID: " + packageID;
+    }
 }
