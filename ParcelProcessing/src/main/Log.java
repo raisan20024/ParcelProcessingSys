2d55e267408b5da57dc702b2c4b5cba3ad2c09ce
raisan20024 <raisan20024@gmail.com>
2024-11-25 11:45:43 +0545

    Added attributes inside Log class

--- ParcelProcessing/src/main/Log.java
+++ ParcelProcessing/src/main/Log.java
@@ -1,5 +1,39 @@
 package main;
 
 public class Log {
+    private static Log instance;
+    private StringBuffer logBuffer;
+    private Log() {
+        logBuffer = new StringBuffer();
+    }
+    public static synchronized Log getInstance() {
+        if (instance == null) {
+            instance = new Log();
+        }
+        return instance;
+    }
 
+    public void addEvent(String event) {
+        logBuffer.append(event).append("\n");
+    }
+    public void addLog(String event) {
+        addEvent(event);
+    }
+    public String getLog() {
+        return logBuffer.toString();
+    }
+    public void clearLog() {
+        logBuffer.setLength(0);
+    }
+    public void writeLogToFile(String filename) {
+        try (java.io.FileWriter writer = new java.io.FileWriter(filename)) {
+            writer.write(logBuffer.toString());
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public String toString() {
+        return logBuffer.toString();
+    }
 }
